a5c15eda9b1bb7a175deedf13bb3a2a217c4f478
altermaRRin <dev.altermarrin@gmail.com>
2022-12-24 23:49:44 +0300

    articles added

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,25 @@
-import Swiper from 'swiper';
+// import Swiper from 'swiper';
 
+// import 'swiper/css';
+new Swiper('.to-read-slider', {
 
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
 
+    // --- Буллеты --- //
+    type: 'bullets',
+    clickable: true,
+  },
+  slidesPerView: 1,
+  watchOverflow: true,
+  loopedSlides: 0,
+
+})
+
+const video =  document.querySelector('iframe')
+document.querySelector('.video-wrapper__btn').addEventListener('click', (e) => {
+  e.preventDefault()
+  video.style.zIndex = 1
+  video.src += 'autoplay=1'
+})
