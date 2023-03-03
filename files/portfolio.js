1e0780bcfbbb23bf5545987e956987b0e490d36b
altermaRRin <dev.altermarrin@gmail.com>
2023-03-03 13:05:51 +0300

    Updates

---
+++ files/portfolio.js
@@ -0,0 +1,35 @@
+export const DATA = [
+  {
+    name: 'iHerb',
+    imgSrc: './img/works/iherb.png',
+    imgAlt: 'iHerb',
+    about: 'Доставка iHerb в Россию',
+    link: 'https://DEValtermaRRin.github.io/iherb',
+    tags: ['статичный сайт', 'создание сайта', 'микроанимация'],
+  },
+  {
+    name: 'Luxerious Fashion',
+    imgSrc: './img/works/shop.png',
+    imgAlt: 'Fashion',
+    about: 'Интернет-магазин одежды',
+    link: 'https://devaltermarrin.github.io/shop/',
+    tags: ['адаптивный сайт', 'создание сайта', 'микроанимация', 'семантические теги'],
+  },
+
+  // {
+  //   name: 'chat',
+  //   imgSrc: './img/works/shop.png',
+  //   imgAlt: 'chat',
+  //  about: 'Реализация чата с авторизацией,
+  //   link: 'https://devaltermarrin.github.io/shop/',
+  //   tags: ['React', 'JS', 'SPA'],
+  // },
+  {
+    name: 'Snake game',
+    imgSrc: './img/works/snake-game.png',
+    imgAlt: 'snake-game',
+    about: 'Реализация игры змейка',
+    link: ' https://DEValtermaRRin.github.io/snake',
+    tags: ['игра', 'JS', 'HTML5'],
+  },
+];
